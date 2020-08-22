f29e38429619b656836102601b762743b1e8894d
me <17600398335@163.com>
2020-08-22 18:26:38 +0800

    优化

--- spring-boot-demo/src/main/java/com/zld/demo/mapper/SingerMapper.java
+++ spring-boot-demo/src/main/java/com/zld/demo/mapper/SingerMapper.java
@@ -1,14 +1,11 @@
 package com.zld.demo.mapper;
 
 import com.zld.demo.pojo.Singer;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-@Mapper
 public interface SingerMapper {
-    int insertSinger(@Param("singerList") List<Singer> record);
+    int insertSinger(List<Singer> list);
 
     int insertSelective(Singer record);
 
--- spring-boot-demo/src/main/java/com/zld/demo/service/serviceImpl/SingerServiceImpl.java
+++ spring-boot-demo/src/main/java/com/zld/demo/service/serviceImpl/SingerServiceImpl.java
@@ -6,11 +6,12 @@ import com.zld.demo.service.SingerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class SingerServiceImpl implements SingerService {
-    @Autowired(required = false)
+    @Resource
     private SingerMapper singerMapper;
 
     @Override
